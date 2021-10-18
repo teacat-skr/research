6692a673998f118de26ebf03707e683b1e5dabc5
teacat-skr <railcore03@gmail.com>
2021-10-18 11:14:06 +0900

    update

--- workspace/research/dd.py
+++ workspace/research/dd.py
@@ -11,12 +11,9 @@ import matplotlib.pyplot as plt
 import csv 
 import warnings
 
-global 
-
 def main():
     epochs = 100
     grad_steps = 500000
-    warnings.filterwarnings('ignore')
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
@@ -39,25 +36,76 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.1)
     x = []
-    y = []
+    tracc = []
+    teacc = []
     x.append(0)
-    y.append(0)
+    tracc.append(0)
+    teacc.append(0)
     data_save = []
-    # for epoch in range(epochs):
-    #     train_acc, train_loss = train(model, device, train_loader, criterion, optimizer)
-    #     test_acc, test_loss = test(model, device, test_loader, criterion)
-    #     stdout_temp = 'epoch: {:>3}, train acc: {:<8}, train loss: {:<8}, test acc: {:<8}, test loss: {:<8}'
-    #     print(stdout_temp.format(epoch+1, train_acc, train_loss, test_acc, test_loss))
-    #     print('')
-    #     data_save.append([epoch,test_acc])
-    #     x.append(epoch + 1)
-    #     y.append(test_acc)
-    step = 0
-    while (grad_steps != step):
+    for epoch in range(epochs):
+        train_acc, train_loss = train(model, device, train_loader, criterion, optimizer)
+        test_acc, test_loss = test(model, device, test_loader, criterion)
+        stdout_temp = 'epoch: {:>3}, train acc: {:<8}, train loss: {:<8}, test acc: {:<8}, test loss: {:<8}'
+        print(stdout_temp.format(epoch+1, train_acc, train_loss, test_acc, test_loss))
+        print('')
+        data_save.append([epoch,test_acc])
+        x.append(epoch + 1)
+        tracc.append(train_acc)
+        teacc.append(test_acc)
+
+    
+    with open('resnet18-cifat10.csv','w') as file:
+        writer = csv.writer(file)
+        writer.writerows(data_save)
+    
+    plt.title("ResNet18 trained by Cifar10")
+    plt.xlim(0, epochs * 1.2)
+    plt.ylim(0, 1)
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.plot(x, tracc, label = 'train')
+    plt.plot(x, teacc, label = 'test')
+    plt.legend(loc='upper right')
+    plt.savefig("sample.png")
+    
+def sub():
+    grad_steps = 500000
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        # transforms.RandomHorizontalFlip,
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, num_workers=2)
+    test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_train)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=128, shuffle=True, num_workers=2)
+    class_names = ('plane', 'car', 'bird', 'cat', 'dog', 'frog', 'ship', 'truck')
+    model = torchvision.models.resnet18(pretrained=False)
+    model = model.to(device)
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+    count = 0
+    x = range(grad_steps + 1)
+    tracc = []
+    teacc = []
+    tracc.append(0)
+    teacc.append(0)
+    data_save = []
+    model.train()
+    while(grad_steps != count):
         output_list = []
         target_list = []
         running_loss = 0.0
         for batich_idx, (inputs, targets) in enumerate(train_loader):
+            if(grad_steps == count):
+                break
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = model(inputs)
             loss = criterion(outputs, targets)
@@ -71,38 +119,25 @@ def main():
             running_loss += loss.item()
 
             train_acc, train_loss = calc_score(output_list, target_list, running_loss, train_loader)
-            if batich_idx % 10 == 0 and batich_idx != 0:
-                stdout_temp = 'gradient step: {:>3}/{:<3}, train acc:{:<8}, train loss: {:<8}'
-                print(stdout_temp.format(batich_idx, grad_steps, train_acc, train_loss))
-            step += 1
-            if step == grad_steps:
-
-        train_acc, train_loss = calc_score(output_list, target_list, running_loss, train_loader)
-            
-        test_acc, test_loss = calc_score(output_list, target_list, running_loss, test_loader)
-        stdout_temp = 'epoch: {:>3}, train acc: {:<8}, train loss: {:<8}, test acc: {:<8}, test loss: {:<8}'
-        print(stdout_temp.format(epoch+1, train_acc, train_loss, test_acc, test_loss))
-        print('')
-        data_save.append([epoch,test_acc])
-        x.append(grad_steps + 1)
-        y.append(test_acc)
-
-
-
-    with open('resnet18-cifat10.csv','w') as file:
-        writer = csv.writer(file)
-        writer.writerows(data_save)
+            if count % 100 == 0 and count != 0:
+                stdout_temp = 'step: {:>3}/{:<3}, train acc:{:<8}, train loss: {:<8}'
+                print(stdout_temp.format(count, grad_steps, train_acc, train_loss))
+            tracc.append(train_acc)
+            count += 1
+    
+    # with open('resnet18-cifat10.csv','w') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerows(data_save)
     
     plt.title("ResNet18 trained by Cifar10")
-    plt.xlim(0, epochs * 1.2)
+    plt.xlim(0, grad_steps * 1.2)
     plt.ylim(0, 1)
-    plt.xlabel("Epochs")
-    plt.ylabel("Test Accuracy")
-    plt.plot(x, y)
-    plt.savefig("sample.png")
-    #test
-    
-    
+    plt.xlabel("Steps")
+    plt.ylabel("Accuracy")
+    plt.plot(x, tracc)
+    plt.savefig("sample1.png")
+
+
     
 
 def train (model, device, train_loader, criterion, optimizer):
@@ -156,11 +191,14 @@ def test(model, device, test_loader, criterion):
 def calc_score(output_list, target_list, running_loss, data_loader):
     # import pdb;pdb.set_trace()
     result = classification_report(output_list, target_list, output_dict=True)
-    acc = round(result['weighted avg']['f1-score'], 6)
+    acc = round(result['accuracy'], 6)
+    table = classification_report(output_list, target_list)
     loss = round(running_loss / len(data_loader.dataset), 6)
 
     return acc, loss
 
     
 if __name__ =='__main__':
-    main()
+    warnings.filterwarnings('ignore')
+    # main()
+    sub()
