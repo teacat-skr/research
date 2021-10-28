c091328aba139a383680ebf3cb8cbc888a06fcd7
teacat-skr <railcore03@gmail.com>
2021-10-28 15:46:52 +0900

    update

--- workspace/research/dd.py
+++ workspace/research/dd.py
@@ -88,6 +88,12 @@ def sub():
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    #インスタンス変数にアクセスしてラベルの張替え
+    for i in range(len(train_set.targets)):
+        if(random.randint(0, 9999) < int(label_noise_rate * 10000)):
+            train_set.targets[i] += random.randint(1, 9)
+            train_set.targets[i] %= 10
+    
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, num_workers=2)
     test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_train)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=128, shuffle=True, num_workers=2)
@@ -222,8 +228,8 @@ def calc_score(output_list, target_list, running_loss, data_loader):
 def parse_args():
     arg_parser = argparse.ArgumentParser(description="ResNet trained by CIFAR-10")
     
-    arg_parser.add_argument("--model_width", nargs="?", type=int, default=1)
-    arg_parser.add_argument("grad_steps", nargs="?", type=int, default=500000)
+    arg_parser.add_argument("-k", "--model_width", nargs="?", type=int, default=1)
+    arg_parser.add_argument("grad_steps", nargs="?", type=int, default=50000)
     arg_parser.add_argument("label_noise_rate", nargs="?", type=float, default=0)
 
     return arg_parser.parse_args()
@@ -232,6 +238,6 @@ if __name__ =='__main__':
     warnings.filterwarnings('ignore')
     # main()
     import time
-    # start = time.perf_counter()
+    start = time.perf_counter()
     sub()
-    # print(time.perf_counter() - start)
+    print(time.perf_counter() - start)
